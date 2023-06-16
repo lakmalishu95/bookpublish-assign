03733ba00d62026a8236f63ab068727f915fb101
lakmalishu95 <isurulakmal858@gmail.com>
2023-06-16 10:13:55 +0530

    first commit

---
+++ src/main/java/com/bookpublish/bookpublish/BookpublishApplication.java
@@ -0,0 +1,13 @@
+package com.bookpublish.bookpublish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookpublishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookpublishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookpublish/bookpublish/BookpublishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookpublish.bookpublish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookpublishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
